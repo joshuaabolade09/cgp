545e91d9809ca86a6364a951156346ec01e000ba
joshuaabolade09 <joshuaabolade09@gmail.com>
2022-11-15 14:45:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// Get the elements from HTML
+
+//Points of Each grade// 
 let A=5
 let B=4
-let K=3
+let C=3
 let D=2
 let E=1
 let F=0
 
+//Global variable and DOM manipulation of each elements//
 
 let form = document.querySelector(".form");
 let GP=document.querySelector('.display-GPA')
@@ -20,7 +22,7 @@ let cd = form.querySelectorAll(".point");
 let displayGPA = document.querySelector(".display-GPA");
 let k=document.querySelector('#su')
 
-let C=document.querySelector('#wem')
+let K=document.querySelector('#wem')
 let bt=document.querySelector('.submit')
 localStorage.setItem("map",JSON.stringify([]))
 localStorage.setItem("set",JSON.stringify([]))
@@ -30,6 +32,8 @@ localStorage.setItem("set",JSON.stringify([]))
 // Assign these elements to nothing because we'll use their values later
 
 function Sum(){
+
+   //Looping through the user inputs unit for each courses and pushing it into an array// 
   
   let put=[]
    for (let i = 0; i < s.length; i++) {
@@ -39,20 +43,26 @@ function Sum(){
       return Number(str ) 
   }
   )
-  
+  //Adding every inputs in the created array and initializing it to the total units//
      k=up.reduce((b,c)=>b+c,0)
        document.querySelector('#su').value =k
-  }   
+       console.log(k)
+  }  
+   //Looping through the user inputs scores for each courses and pushing it into an array// 
   let ut=[]
   for (let c = 0; c < b.length; c++) {
   ut.push(b[c].value)
   let pup=ut.map(stm=>{
   return Number(stm)
+   
   })
+   //Adding every score inputs in the created array and initializing it to the total  score units//
   let j=pup.reduce((d,e)=>d+e,0)
-   C.value=j
+   K.value=j
      let r=[j]
 
+
+     //Looping through each score inputs and assigning grade to each score // 
     for (let up = 0; up < ore.length; up++) {
       const lement = ore [ up];
   ut.forEach(function(movement,index){
@@ -78,6 +88,8 @@ function Sum(){
                  ore[index].value="F"
               }
                for (let ind = 0; ind < cd.length; ind++) {
+
+            //Calculating each points by multiplying each grade points of each scores by their units//
      const ele= cd[ind];
       if(ore[index].value==="A"){
          cd[index].value= A * put[index]
@@ -86,7 +98,7 @@ function Sum(){
         cd[index].value= B * put[index]
          }
      else if(ore[index].value=="C"){
-      cd[index].value= K * put[index]
+      cd[index].value= C * put[index]
   } 
   else if(ore[index].value==="D"){
      cd[index].value= D* put[index]              
@@ -97,15 +109,21 @@ function Sum(){
      else if(ore[index].value==="F"){
         cd[index].value= F* put[index]
            }
+         //Put each point value into an array//
            let ck=[cd[0].value,cd[1].value,cd[2].value,cd[3].value,cd[4].value,cd[5].value,cd[6].value,cd[7].value,cd[8].value
            , cd[9].value,cd[10].value]
+           //Convert the values from string to Number//
              const pushing=ck.map(sem=>{
                 return Number(sem)
               }
               
             )
+            //Add the numbers together and initialize it to the total point//
+
              re=pushing.reduce((s,p)=>s+p,0)
              document.querySelector("#mee").value=re
+             console.log(re)
+            
              
 
 
@@ -115,20 +133,26 @@ function Sum(){
      }
   }) 
 
+  //Initializing the local storage//
 
-
-  }
-  }
   const fre=JSON.parse(localStorage.getItem("set"))
 const free=JSON.parse(localStorage.getItem("map"))
-fre.push(k)
 
+//Pushing the total units into the local storage//
+fre.push(k)
+//Pushing the total points into the local storage//
 free.push(re)
 localStorage.setItem("map",JSON.stringify(free))
 localStorage.setItem("set",JSON.stringify(fre))
-console.log(Number(free))
+
+
+
+  }
   }
   
+  }
+  
+  //SEt every user inputs to empty string//
  
   function calculate(){
    document.querySelector('#su').value=""
